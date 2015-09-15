5bc0d89138da74bf765b26926814ae9fcdbc110e
jboeshart <jason.boeshart@perceptivesoftware.com>
2015-09-14 21:58:57 -0500

    Updated metadata for supermarket info

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,15 @@
 name             'aws-ec2-snapshot'
 maintainer       'Jason Boeshart'
 maintainer_email 'jason.boeshart@gmail.com'
-license          'GNU General Public License v2.0'
+license          'GPL v2'
 description      'Installs/Configures aws-ec2-snapshot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/jboeshart/aws-ec2-snapshot'
+supports         'centos'
+supports         'rhel'
+supports         'fedora'
+supports         'windows'
 version          '1.0.0'
 
 depends          "windows", ">= 1.37.0"
+depends          "awscli", ">= 1.1.1"
